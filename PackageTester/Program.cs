42dee19939512cfa50573e2c556c7f80a31672a2
tmenier <tmenier@gmail.com>
2014-05-05 10:47:33 -0500

    fixed some Flurl.Http packaging issues, upgraded some packages, added app for testing packages

---
+++ PackageTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Flurl.Http;
+using Flurl.Http.Testing;
+
+namespace PackageTester
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			Console.WriteLine("http://www.google.com".GetStringAsync().Result);
+			Console.WriteLine("^-- real response");
+			using (var test = new HttpTest()) {
+				test.RespondWith("totally fake google source");
+				Console.WriteLine("http://www.google.com".GetStringAsync().Result);
+				Console.WriteLine("^-- fake response");
+			}
+			var path = "http://www.google.com".DownloadFileAsync("c:\\flurl", "google.txt").Result;
+			Console.WriteLine("dowloaded google source to " + path);
+			Console.ReadLine();
+		}
+	}
+}
