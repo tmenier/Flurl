e5edd5e27fd7ceb9526bbdb673829f87163661dd
tmenier <tmenier@gmail.com>
2014-05-12 11:49:12 -0500

    tweaks to nuspecs, AssemblyInfo, post-build

--- Flurl.Http/Properties/AssemblyInfo.cs
+++ Flurl.Http/Properties/AssemblyInfo.cs
@@ -1,28 +1,6 @@
 ﻿using System.Reflection;
-using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Flurl.Http")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Flurl.Http")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Simple, testable HTTP calls off Flurl's fluent URL builder.")]
+[assembly: AssemblyCopyright("Copyright © Todd Menier 2014")]
+[assembly: AssemblyVersion("0.1.2")]
