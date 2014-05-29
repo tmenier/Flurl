b8c83f7723163a6d52d0ca595db0c72e0424efe4
Ben Ellis <ben@oldtownit.com>
2014-05-29 15:32:57 -0500

    Add extensions for HEAD calls
    basic support for HTTP HEAD method

---
+++ Flurl.Http/HeadExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Flurl.Http
+{
+	public static class HeadExtensions
+	{
+		/// <summary>
+		/// Sends an asynchronous HEAD request.
+		/// </summary>
+		/// <returns>A Task whose result is the received HttpResponseMessage.</returns>
+		public static Task<HttpResponseMessage> HeadAsync(this FlurlClient client)
+		{
+			HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Head, client.Url);
+			return client.HttpClient.SendAsync(message);
+		}
+
+		/// <summary>
+		/// Creates a FlurlClient from the URL and sends an asynchronous HEAD request.
+		/// </summary>
+		/// <returns>A Task whose result is the received HttpResponseMessage.</returns>
+		public static Task<HttpResponseMessage> HeadAsync(this string url)
+		{
+			return new FlurlClient(url).HeadAsync();
+		}
+
+		/// <summary>
+		/// Creates a FlurlClient from the URL and sends an asynchronous HEAD request.
+		/// </summary>
+		/// <returns>A Task whose result is the received HttpResponseMessage.</returns>
+		public static Task<HttpResponseMessage> HeadAsync(this Url url)
+		{
+			return new FlurlClient(url).HeadAsync();
+		}
+	}
+}
---
+++ Flurl.Test/Http/HeadTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Flurl.Http.Testing;
+using NUnit.Framework;
+
+namespace Flurl.Test.Http
+{
+	[TestFixture]
+	public class HeadTests
+	{
+		[Test]
+		public async Task can_call_head_for_flurlclient()
+		{
+			var test = new HttpTest();
+
+			await "http://www.api.com".HeadAsync();
+
+			Assert.AreEqual(1, test.CallLog.Count);
+			Assert.AreEqual(HttpMethod.Head, test.CallLog.Single().Request.Method);
+		}
+
+		[Test]
+		public async Task can_call_head_for_string_url()
+		{
+			var test = new HttpTest();
+
+			await "http://www.api.com".HeadAsync();
+
+			Assert.AreEqual(1, test.CallLog.Count);
+			Assert.AreEqual(HttpMethod.Head, test.CallLog.Single().Request.Method);
+		}
+
+		[Test]
+		public async Task can_call_head_for_url()
+		{
+			var test = new HttpTest();
+
+			await new Url("http://www.api.com").HeadAsync();
+
+			Assert.AreEqual(1, test.CallLog.Count);
+			Assert.AreEqual(HttpMethod.Head, test.CallLog.Single().Request.Method);
+		}
+	}
+}
