cf4fde626f6948d7ca3e619c003dc709f51c67d4
tmenier <tmenier@gmail.com>
2014-06-07 13:45:22 -0500

    shared AssemblyInfo.cs for Flurl.Http

--- Flurl.Http.PCL/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Flurl.Http")]
-[assembly: AssemblyDescription("Simple, testable HTTP calls off Flurl's fluent URL builder.")]
-[assembly: AssemblyCopyright("Copyright © Todd Menier 2014")]
-[assembly: AssemblyVersion("0.1.2")]
---
+++ Flurl.Http.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Flurl.Http")]
+[assembly: AssemblyDescription("A fluent, testable HTTP client library that extends Flurl's URL builder chain.")]
+[assembly: AssemblyCopyright("Copyright © Todd Menier 2014")]
+[assembly: AssemblyVersion("0.2.0")]
