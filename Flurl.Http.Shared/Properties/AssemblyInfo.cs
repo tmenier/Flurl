1cc07e69be0b5edff9a4abce5220c524fbd9f53d
tmenier <tmenier@gmail.com>
2015-03-17 16:15:22 -0500

    up'd Flurl.Http to 0.5.2

--- Flurl.Http.Shared/Properties/AssemblyInfo.cs
+++ Flurl.Http.Shared/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Flurl.Http")]
-[assembly: AssemblyDescription("A fluent, testable HTTP client library that extends Flurl's URL builder chain.")]
+[assembly: AssemblyDescription("A fluent, testable, wrist-friendly HTTP client library.")]
 [assembly: AssemblyCopyright("Copyright © Todd Menier 2015")]
-[assembly: AssemblyVersion("0.5.1")]
+[assembly: AssemblyVersion("0.5.2")]
