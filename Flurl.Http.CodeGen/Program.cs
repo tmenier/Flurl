5567bcad941284e51276c1ddd8a289444fbf190e
tmenier <tmenier@gmail.com>
2015-03-22 10:51:35 -0500

    set up code generator project ahead of new features (xml, cancelation tokens, more verbs) because # of extension methods is about to explode

---
+++ Flurl.Http.CodeGen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flurl.Http.CodeGen
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			if (args.Length == 0)
+				throw new ArgumentException("Must provide a path to the .cs output file.");
+
+			var codePath = args[0];
+			using (var writer = new CodeWriter(codePath)) {
+				writer
+					.WriteLine("using System.Net.Http;")
+					.WriteLine("using System.Threading.Tasks;")
+					.WriteLine("using Flurl.Http.Content;")
+					.WriteLine("")
+					.WriteLine("namespace Flurl.Http")
+					.WriteLine("{")
+					.WriteLine("public static class HttpExtensions")
+					.WriteLine("{");
+
+				writer.WriteLine("// extension methods here");
+
+				writer
+					.WriteLine("}")
+					.WriteLine("}");
+			}
+		}
+	}
+}
---
+++ Flurl.Http.Shared/HttpExtensions.cs
@@ -0,0 +1,11 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http.Content;
+
+namespace Flurl.Http
+{
+	public static class HttpExtensions
+	{
+		// extension methods here
+	}
+}
