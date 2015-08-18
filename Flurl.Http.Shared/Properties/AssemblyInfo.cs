560a6c7d629e2f236b90eb2bfc828077866f861d
tmenier <tmenier@gmail.com>
2015-08-17 20:45:10 -0500

    up'd flurl.http to 0.6.4

--- Flurl.Http.Shared/Properties/AssemblyInfo.cs
+++ Flurl.Http.Shared/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Flurl.Http")]
 [assembly: AssemblyDescription("A fluent, testable, wrist-friendly HTTP client library.")]
 [assembly: AssemblyCopyright("Copyright © Todd Menier 2015")]
-[assembly: AssemblyVersion("0.6.3")]
+[assembly: AssemblyVersion("0.6.4")]
