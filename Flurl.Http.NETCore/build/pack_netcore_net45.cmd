2e76c8a3e9abd30469546cd6252ccf0b9102f699
Nikolay Molchanov <me@kroniak.net>
2016-03-19 04:18:32 +0300

    Added NETCore library, build cmd, nuget pack is ready

---
+++ Flurl.Http.NETCore/build/pack_netcore_net45.cmd
@@ -0,0 +1,3 @@
+cd ..
+call dnu restore
+call dnu pack --configuration Release --framework net45 --framework dotnet5.4 
---
+++ Flurl.Http.NETCore/build/pack_netcore_net45_net46.cmd
@@ -0,0 +1,3 @@
+cd ..
+call dnu restore
+call dnu pack --configuration Release --framework net45 --framework net46 --framework dotnet5.4 
