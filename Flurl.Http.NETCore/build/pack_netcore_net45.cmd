41f6b03889777a982d2a8bbc49ae4ea8cd64c914
Nikolay Molchanov <me@kroniak.net>
2016-03-20 13:48:33 +0300

    [edit] targets, packages versions

--- Flurl.Http.NETCore/build/pack_netcore_net45.cmd
+++
@@ -1,3 +0,0 @@
-cd ..
-call dnu restore
-call dnu pack --configuration Release --framework net45 --framework dotnet5.4 
--- Flurl.Http.NETCore/build/pack_netcore_net45_net46.cmd
+++
@@ -1,3 +0,0 @@
-cd ..
-call dnu restore
-call dnu pack --configuration Release --framework net45 --framework net46 --framework dotnet5.4 
