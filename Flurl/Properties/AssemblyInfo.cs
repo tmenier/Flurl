52ab5ab8419de2ac3cc9488f2ae808a5b00be13f
tmenier <tmenier@gmail.com>
2016-05-15 10:06:41 -0500

    Flurl 2.0

--- Flurl/Properties/AssemblyInfo.cs
+++ Flurl/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Flurl")]
 [assembly: AssemblyDescription("A fluent, portable URL builder.")]
 [assembly: AssemblyCopyright("Copyright © Todd Menier 2016")]
-[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyVersion("2.0.0")]
