1b7d6de993086711b61710b7666b78e909fb4d3b
Nikolay Molchanov <me@kroniak.net>
2017-07-17 11:23:18 +0300

    [fix] flurl nuget pack

--- Build/build.cmd
+++ Build/build.cmd
@@ -17,6 +17,15 @@ echo Error! Generation cs file failed.
 exit /b 1
 )
 
+@set project=..\src\Flurl\
+
+@call dotnet build -c Release %project%
+
+@if ERRORLEVEL 1 (
+echo Error! Build Flurl failed.
+exit /b 1
+)
+
 @set project=..\src\Flurl.Http\
 
 @call dotnet build -c Release %project%
