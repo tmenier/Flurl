3e44c2ea443114b95fc3ce95191394d502b5d8a1
Todd Menier <tmenier@gmail.com>
2017-08-17 16:43:04 -0500

    build folder in sln

--- Build/build.cmd
+++ Build/build.cmd
@@ -11,7 +11,7 @@ echo Restoring dependicies was successful.
 
 @set project=..\src\Flurl.Http.CodeGen\Flurl.Http.CodeGen.csproj
 
-@call dotnet run -c Release -p %project% ..\src\Flurl.Http\AutoGenExtensions.cs
+@call dotnet run -c Release -p %project% ..\src\Flurl.Http\GeneratedExtensions.cs
 @if ERRORLEVEL 1 (
 echo Error! Generation cs file failed.
 exit /b 1
