01ad1ce7761f21cfcfadfd36768165f5b06c437a
Todd Menier <tmenier@gmail.com>
2017-08-24 20:38:39 -0500

    added URL builder extensions on IFlurlRequest

--- Test/Flurl.Test/Http/FlurlClientTests.cs
+++ Test/Flurl.Test/Http/FlurlClientTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using Flurl.Http;
 using NUnit.Framework;
@@ -11,7 +12,10 @@ namespace Flurl.Test.Http
 		[Test]
 		// check that for every FlurlClient extension method, we have an equivalent Url and string extension
 		public void extension_methods_consistently_supported() {
-			var frExts = ReflectionHelper.GetAllExtensionMethods<IFlurlRequest>(typeof(FlurlClient).GetTypeInfo().Assembly).ToList();
+			var frExts = ReflectionHelper.GetAllExtensionMethods<IFlurlRequest>(typeof(FlurlClient).GetTypeInfo().Assembly)
+				// URL builder methods on IFlurlClient get a free pass. We're looking for things like HTTP calling methods.
+				.Where(mi => mi.DeclaringType != typeof(UrlBuilderExtensions))
+				.ToList();
 			var urlExts = ReflectionHelper.GetAllExtensionMethods<Url>(typeof(FlurlClient).GetTypeInfo().Assembly).ToList();
 			var stringExts = ReflectionHelper.GetAllExtensionMethods<string>(typeof(FlurlClient).GetTypeInfo().Assembly).ToList();
 
