32b6f013eb2be571333de2db8f5cbb1069d20ff9
Todd Menier <tmenier@gmail.com>
2017-09-04 12:27:45 -0500

    updated package testers for Flurl.Http-pre1

--- PackageTesters/PackageTester.NETCore/Program.cs
+++ PackageTesters/PackageTester.NETCore/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using Flurl.Http;
 
 namespace PackageTester
 {
 	public class Program
 	{
 		public static void Main(string[] args) {
-			var client = new FlurlClient().EnableCookies();
-			//client.Request("https://httpbin.org/cookies/set?z=999").HeadAsync().Wait();
-			Console.WriteLine("999" == client.Cookies["z"].Value);
+			new Tester().DoTestsAsync().Wait();
 			Console.ReadLine();
-
-			//new Tester().DoTestsAsync().Wait();
-			//Console.ReadLine();
 		}
 	}
 }
