0dac633b58221dc424d22a509d7b5d346b162a7d
Todd Menier <tmenier@gmail.com>
2017-10-09 13:34:00 -0500

    simplified test for #233

--- Test/Flurl.Test/Http/DefaultUrlEncodedSerializerTests.cs
+++ Test/Flurl.Test/Http/DefaultUrlEncodedSerializerTests.cs
@@ -11,45 +11,17 @@ namespace Flurl.Test.Http
     [TestFixture, Parallelizable]
     public class DefaultUrlEncodedSerializerTests
     {
-        public class Grant
-        {
-            public string grant_type { get; set; }
-            public string username { get; set; }
-            public string password { get; set; }
-            public string refresh_token { get; set; }
-            public string client_id { get; set; }
-            public string client_secret { get; set; }
-            public string scope { get; set; }
-        }
+	    [Test]
+	    public void can_serialize_object() {
+		    var vals = new {
+			    a = "foo",
+			    b = 333,
+			    c = (string)null, // exlude
+			    d = ""
+		    };
 
-        public class Grant_Types
-        {
-            public const string password = "password";
-            public const string refresh_token = "refresh_token";
-            public const string client_credentials = "client_credentials";
-        }
-
-        public class Grant_Scopes
-        {
-            public const string password = "openid offline_access";
-            public const string client_credentials = "openid";
-        }
-
-        [Test]
-        public void can_serialize_with_grant_class()
-        {
-            var serializer = new DefaultUrlEncodedSerializer();
-            var result = serializer.Serialize(new Grant
-            {
-                grant_type = Grant_Types.password,
-                username = "user",
-                password = "password",
-                client_id = "Client_Id",
-                client_secret = "Client_Secret",
-                scope = Grant_Scopes.password
-            });
-
-            Assert.AreEqual("grant_type=password&username=user&password=password&client_id=Client_Id&client_secret=Client_Secret&scope=openid+offline_access", result.ToString());
-        }
-    }
+		    var serialized = new DefaultUrlEncodedSerializer().Serialize(vals);
+		    Assert.AreEqual("a=foo&b=333&d=", serialized);
+	    }
+	}
 }
