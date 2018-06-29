e71a4f69f9541eacbd1d7c4573b13716600efc67
Thiago Lunardi <thiago.lunardi@complevo.de>
2018-06-29 10:34:29 +0200

    remove unused code

--- src/Flurl/Url.cs
+++ src/Flurl/Url.cs
@@ -306,18 +306,6 @@ namespace Flurl
             return this;
 		}
 
-        private static bool TryGetAttribute<T>(MemberInfo memberInfo, out T customAttribute) where T : Attribute
-        {
-            var attributes = memberInfo.GetCustomAttributes(typeof(T), false).FirstOrDefault();
-            if (attributes == null)
-            {
-                customAttribute = null;
-                return false;
-            }
-            customAttribute = (T)attributes;
-            return true;
-        }
-
         /// <summary>
         /// Adds multiple parameters without values to the query.
         /// </summary>
