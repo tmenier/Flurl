925a05ff5fcbd2cb62d00a76dbed9d2cc05c45ff
Todd Menier <tmenier@gmail.com>
2018-07-27 15:58:20 -0500

    #356 IFlurlClientFactory.ConfigureClient

---
+++ Test/Flurl.Test/Http/FlurlClientFactoryTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl.Http.Configuration;
+using NUnit.Framework;
+
+namespace Flurl.Test.Http
+{
+	[TestFixture]
+    public class FlurlClientFactoryTests
+    {
+	    [Test]
+	    public void per_host_factory_provides_same_client_per_host() {
+		    var fac = new PerHostFlurlClientFactory();
+		    var cli1 = fac.Get("http://api.com/foo");
+		    var cli2 = fac.Get("https://api.com/bar");
+		    Assert.AreSame(cli1, cli2);
+	    }
+
+	    [Test]
+	    public void per_base_url_factory_provides_same_client_per_provided_url() {
+		    var fac = new PerBaseUrlFlurlClientFactory();
+		    var cli1 = fac.Get("http://api.com/foo");
+		    var cli2 = fac.Get("http://api.com/bar");
+		    var cli3 = fac.Get("http://api.com/foo");
+		    Assert.AreNotSame(cli1, cli2);
+		    Assert.AreSame(cli1, cli3);
+	    }
+
+	    [Test]
+	    public void can_configure_client_from_factory() {
+		    var fac = new PerHostFlurlClientFactory();
+		    fac.ConfigureClient("http://api.com/foo", c => c.Settings.CookiesEnabled = true);
+		    Assert.IsTrue(fac.Get("https://api.com/bar").Settings.CookiesEnabled);
+		    Assert.IsFalse(fac.Get("http://api2.com/foo").Settings.CookiesEnabled);
+	    }
+
+	    [Test]
+	    public void ConfigureClient_is_thread_safe() {
+		    var fac = new PerHostFlurlClientFactory();
+
+		    var x = "";
+
+		    var t1 = Task.Run(() => fac.ConfigureClient("http://api.com", c => {
+			    x = "in thread 1";
+			    Thread.Sleep(200);
+			    Assert.AreEqual("in thread 1", x);
+			    x = "still in thread 1";
+		    }));
+
+		    Thread.Sleep(100);
+		    var t2 = Task.Run(() => fac.ConfigureClient("http://api.com", c => {
+			    Assert.AreEqual("still in thread 1", x);
+			    x = "in thread 2";
+		    }));
+
+		    Task.WaitAll(t1, t2);
+		    Assert.AreEqual("in thread 2", x);
+	    }
+	}
+}
--- src/Flurl.Http/Configuration/IFlurlClientFactory.cs
+++ src/Flurl.Http/Configuration/IFlurlClientFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace Flurl.Http.Configuration
 {
@@ -14,5 +15,30 @@ namespace Flurl.Http.Configuration
 		/// <param name="url">The URL being called.</param>
 		/// <returns></returns>
 		IFlurlClient Get(Url url);
+	}
+
+	/// <summary>
+	/// Extension methods on IFlurlClientFactory
+	/// </summary>
+	public static class FlurlClientFactoryExtensions
+	{
+		// https://stackoverflow.com/questions/51563732/how-do-i-lock-when-the-ideal-scope-of-the-lock-object-is-known-only-at-runtime
+		// a client's hashcode isn't guananteed unique, but in the unlikely event of a collision, it just means you can't configure
+		// 2 clients at the same time, which though not ideal is totally safe.
+		private static ConcurrentDictionary<int, object> _clientLocks = new ConcurrentDictionary<int, object>();
+
+		/// <summary>
+		/// Provides thread-safe access to a specific IFlurlClient, typically to configure settings and default headers.
+		/// The URL is used to find the client, but keep in mind that the same client will be used in all calls to the same host by default.
+		/// </summary>
+		/// <param name="factory">This IFlurlClientFactory.</param>
+		/// <param name="url">the URL used to find the IFlurlClient.</param>
+		/// <param name="configAction">the action to perform against the IFlurlClient.</param>
+		public static void ConfigureClient(this IFlurlClientFactory factory, string url, Action<IFlurlClient> configAction) {
+			var client = factory.Get(url);
+			lock (_clientLocks.GetOrAdd(client.GetHashCode(), new object())) {
+				configAction(client);
+			}
+		}
 	}
 }
