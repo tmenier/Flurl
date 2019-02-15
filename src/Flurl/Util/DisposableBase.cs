e78701d4827b7ce15799b2a60689fdd805c71124
hanabi1224 <harlowmoo@gmail.com>
2019-02-15 12:19:03 +0800

    fix docs

--- src/Flurl/Util/DisposableBase.cs
+++ src/Flurl/Util/DisposableBase.cs
@@ -2,29 +2,44 @@
 
 namespace Flurl.Util
 {
+    /// <summary>
+    /// Extends IDisposable with a flag indicating wheather object has been disposed
+    /// </summary>
     public interface IDisposableObservable : IDisposable
     {
+        /// <summary>
+        /// Gets a value indicating whether this object has been disposed.
+        /// </summary>
         bool IsDisposed { get; }
     }
 
     /// <summary>
-    /// Base class that follows IDisposable pattern described at https://docs.microsoft.com/en-us/dotnet/api/system.idisposable.dispose?redirectedfrom=MSDN&view=netframework-4.7.2#System_IDisposable_Dispose
+    /// Base class that follows IDisposable pattern
     /// </summary>
     public abstract class DisposableBase : IDisposableObservable
     {
+        /// <summary>
+        /// Finalizes an instance of the <see cref="DisposableBase" /> class.
+        /// </summary>
         ~DisposableBase()
         {
             this.Dispose(false);
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <inheritdoc/>
         public bool IsDisposed { get; private set; }
 
+        /// <summary>
+        /// Dispose resources.
+        /// </summary>
+        /// <param name="disposing">True if called explicitly, false if called through the destructor.</param>
         protected void Dispose(bool disposing)
         {
             if (IsDisposed)
@@ -40,6 +55,9 @@ namespace Flurl.Util
             IsDisposed = true;
         }
 
+        /// <summary>
+        /// Release resources. base.Dispose() should not be called from this method. 
+        /// </summary>
         protected abstract void DisposeResources();
     }
 }
--- src/Flurl/Util/DisposableContainer.cs
+++ src/Flurl/Util/DisposableContainer.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 
 namespace Flurl.Util
 {
+    /// <summary>
+    /// A container that holds a collection of disposable objects and dispose all of them of container's own disposal
+    /// </summary>
     public class DisposableContainer : DisposableBase
     {
         private readonly IList<IDisposable> _itemsToDispose = new List<IDisposable>();
 
+        /// <summary>
+        /// Register a disposable object to the container so that it will be disposed on container's disposal
+        /// </summary>
+        /// <param name="object"></param>
         public void Register(IDisposable @object)
         {
             _itemsToDispose.Add(@object);
         }
 
+        /// <inheritdoc/>
         protected override void DisposeResources()
         {
             foreach (var item in _itemsToDispose)
