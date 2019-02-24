5fef156ad63c126ff41b4ed7edeac751ebb447a7
Nikolay Molchanov <me@kroniak.net>
2019-02-24 19:42:50 +0300

    [edit] add test exclude to test.cmd

--- Build/test.cmd
+++ Build/test.cmd
@@ -1,4 +1,4 @@
-@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75 ..\test\Flurl.Test\
+@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75 /p:Exclude="[NUnit3.*]*" ..\test\Flurl.Test\
 @if ERRORLEVEL 1 (
 echo Error! Tests for Flurl failed.
 exit /b 1
