7bd7345f876e3fdfc75cc6addb556a761e7529df
Nikolay Molchanov <me@kroniak.net>
2019-02-24 12:54:13 +0300

    [fix] try to fix tests

--- Build/test.cmd
+++ Build/test.cmd
@@ -1,3 +1,5 @@
-@cd ..\test\Flurl.Test\
-@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75
-@cd ..\..\Build\
+@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75 ..\test\Flurl.Test\
+@if ERRORLEVEL 1 (
+echo Error! Tests for Flurl failed.
+exit /b 1
+)
