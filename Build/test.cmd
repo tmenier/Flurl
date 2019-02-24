72be7f40c0c6628eed27c0d4d95bf0b8f6a598af
Nikolay Molchanov <me@kroniak.net>
2019-02-24 12:04:41 +0300

    [add] coverage for tests

--- Build/test.cmd
+++ Build/test.cmd
@@ -1,3 +1,3 @@
 @cd ..\test\Flurl.Test\
-@call dotnet test -c Release
+@call dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75 ../test/Flurl.Test/
 @cd ..\..\Build\
--- Build/test.coverage.sh
+++ Build/test.coverage.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -euo pipefail
 
 SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "!!WARNING!! This script for pipeline test running and checking code coverage for netstandard and netcoreapp targets"
+echo "!!WARNING!! This script generates test coverage file and runs tests for netstandard and netcoreapp targets"
 
 dotnet test -c Release /p:CollectCoverage=true /p:Threshold=75 \
 /p:Exclude="[NUnit3.*]*" \
+/p:CoverletOutputFormat=lcov /p:CoverletOutput="${SCRIPT_ROOT}/../../lcov" \
 "${SCRIPT_ROOT}/../test/Flurl.Test/"
