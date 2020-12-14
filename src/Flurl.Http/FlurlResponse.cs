18cf6b418ca18f459dead236c4d8dcc69990774b
Todd <tmenier@gmail.com>
2020-12-13 22:56:35 -0600

    #585

--- src/Flurl.Http/FlurlResponse.cs
+++ src/Flurl.Http/FlurlResponse.cs
@@ -132,7 +132,7 @@ namespace Flurl.Http
 		}
 
 		private IReadOnlyList<FlurlCookie> LoadCookies() {
-			var url = ResponseMessage.RequestMessage.RequestUri.ToString();
+			var url = ResponseMessage.RequestMessage.RequestUri.AbsoluteUri;
 			return ResponseMessage.Headers.TryGetValues("Set-Cookie", out var headerValues) ?
 				headerValues.Select(hv => CookieCutter.ParseResponseHeader(url, hv)).ToList() :
 				new List<FlurlCookie>();
